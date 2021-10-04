ea72d5812edcc5d2d4d7567d12efef1562372708
SeongKil <38479834+HyunSeongKil@users.noreply.github.com>
2021-10-04 14:46:54 +0900

    fix

---
+++ src/main/java/kr/co/sootechsys/scrobot/domain/DeployDto.java
@@ -0,0 +1,15 @@
+package kr.co.sootechsys.scrobot.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeployDto {
+    
+    private String prjctId;
+
+    private String trgetSysId;
+}
