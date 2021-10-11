3ec75ec276024a2c4fabf7b1159855b4eb422064
SeongKil <38479834+HyunSeongKil@users.noreply.github.com>
2021-10-11 15:54:22 +0900

    fix

--- src/main/java/kr/co/sootechsys/scrobot/controller/WordDicaryRestController.java
+++ src/main/java/kr/co/sootechsys/scrobot/controller/WordDicaryRestController.java
@@ -11,37 +11,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.sootechsys.scrobot.service.WordDicaryService;
-
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 용어 사전
  */
 @RestController
 @RequestMapping("/word-dicarys")
+@Slf4j
 public class WordDicaryRestController {
 
-    WordDicaryService service;
-    
-    public WordDicaryRestController(WordDicaryService service){
-        this.service = service;
+  WordDicaryService service;
+
+  public WordDicaryRestController(WordDicaryService service) {
+    this.service = service;
+  }
+
+  /**
+   * 용어별 목록 조회
+   * 
+   * @param words 용어. 구분자:콤마 예)코드,명칭
+   * @return
+   */
+  @GetMapping("/by-words")
+  public ResponseEntity<Map<Integer, Object>> listByWords(@RequestParam String words) {
+    String[] arr = words.split(",");
+
+    Map<Integer, Object> map = new LinkedHashMap<>();
+
+    for (int i = 0; i < arr.length; i++) {
+      // log.debug("{} {}", i, arr[i]);
+      map.put(i, service.findAllByWordNmLike(arr[i]));
     }
 
-
-    /**
-     * 용어별 목록 조회
-     * @param words 용어. 구분자:콤마 예)코드,명칭
-     * @return
-     */
-    @GetMapping("/by-words")
-    public ResponseEntity<Map<String,Object>> listByWords(@RequestParam String words){
-      String[] arr = words.split(",");
-
-      Map<String,Object> map = new LinkedHashMap<>();
-
-      for(int i=0; i<arr.length; i++){
-        map.put(arr[i], service.findAllByWordNmLike(arr[i]));
-      }
-
-      return ResponseEntity.ok(map);
-    }
+    return ResponseEntity.ok(map);
+  }
 }
