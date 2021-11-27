3ae5ac3a009ff2bbdbe99462bb19e063d04cc7a3
SeongKil <38479834+HyunSeongKil@users.noreply.github.com>
2021-11-27 23:19:32 +0900

    프로젝트아이디+부모메뉴아이디로 목록 조회() 추가

--- src/main/java/kr/co/sootechsys/scrobot/persistence/MenuRepository.java
+++ src/main/java/kr/co/sootechsys/scrobot/persistence/MenuRepository.java
@@ -2,10 +2,13 @@ package kr.co.sootechsys.scrobot.persistence;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import kr.co.sootechsys.scrobot.domain.MenuDto;
 import kr.co.sootechsys.scrobot.entity.Menu;
-
 
 /**
  * 메뉴
@@ -19,6 +22,11 @@ public interface MenuRepository extends JpaRepository<Menu, String> {
    * @param prjctId 프로젝트 아이디
    * @return
    */
+  @ApiOperation(value = "프로젝트아이디로 목록 조회")
   List<Menu> findAllByPrjctId(String prjctId);
+
+  @Query(value = "SELECT a FROM Menu a WHERE a.prjctId = :prjctId and a.prntsMenuId = :prntsMenuId ORDER BY a.menuOrdrValue")
+  @ApiOperation(value = "프로젝트아이디+부모메뉴아이디로 목록 조회")
+  List<Menu> findAllByPrjctIdAndPrntsMenuId(@Param("prjctId") String prjctId, @Param("prntsMenuId") String prntsMenuId);
 
 }
